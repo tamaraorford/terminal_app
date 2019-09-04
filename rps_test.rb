a6c0505ccc77b5247fad7f374da73e48f8929752
Tamara Orford <tamara@192-168-1-188.tpgi.com.au>
2019-09-04 14:55:44 +1000

    first test developing

---
+++ rps_test.rb
@@ -0,0 +1,23 @@
+require 'test/unit'
+require_relative 'rps'  # .rb is assumed by Ruby
+
+class RpsTest < Test::Unit::TestCase
+     
+    def test_name
+     
+       #Create a new player name and check that name to ensure it is properly stored
+        #No need to use def set up here as there is only one method
+       player = Player.new("Matt")
+        assert_equal('Matt', player.name)
+    end
+
+    def welcome
+        return  "Hello, #{player}! Ready to play RPS!?!"
+    end
+
+end
+
+
+
+
+    
