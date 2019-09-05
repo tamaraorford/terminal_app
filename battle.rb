3db0a1e6f345340cf4e0f2e4d36db139fbf1b72b
Tamara Orford <tamara@192-168-1-188.tpgi.com.au>
2019-09-05 13:17:53 +1000

    created battle test to check if the return of new_round is wtithin the specified array

--- battle.rb
+++ battle.rb
@@ -13,7 +13,16 @@ class Battle
   
   
     def new_round
-        
-      return 'win'
+        win_rules = {
+            'r' => {'s' => 'win', 'r' => 'draw', 'p' => 'loose'},
+            'p' => {'r' => 'win', 'p' => 'draw', 's' => 'loose'},
+            's' => {'p' => 'win', 's' => 'draw', 'r' => 'loose'}
+        }
+
+        player1_shot = @p1.shoot
+        player2_shot = @p2.shoot
+        player1_result = win_rules[player1_shot][player2_shot]
+        puts "#{player1_shot} vs #{player2_shot} ==> #{@p1.name} #{player1_result}"
+    return player1_result
     end
   end
--- battle_test.rb
+++ battle_test.rb
@@ -6,6 +6,8 @@ require_relative 'Player'
 class Battle_Test < Test::Unit::TestCase
     def test_new_round
         battle = Battle.new(Player.new('tamara'), Player.new('skynet'))
-        assert_equal(battle.new_round, 'win')
+        assert_includes(['win', 'loose', 'draw'], battle.new_round)
     end
+
+
 end
